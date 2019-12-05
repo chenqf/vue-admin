6ad8e4ed0238ce12075fe8baf32949204b77cb58
chenqf <qifeng.chen1006@foxmail.com>
2019-12-05 17:28:54 +0800

    http 模块修改

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ export default {
         PROTOCOL:'http:',
         HOSTNAME:'10.13.1.93',
         PORT:'3001',
+        WITH_CREDENTIALS:true,
         TIMEOUT:3000
     }
 }
--- src/libs/http.js
+++ src/libs/http.js
@@ -7,7 +7,7 @@ import setting from '../config.js'
 axios.interceptors.request.use(function (config) {
     // 在发送请求之前做些什么
     config.baseURL = setting.HTTP.PROTOCOL + '//' + setting.HTTP.HOSTNAME + (setting.HTTP.PORT ? `:${setting.HTTP.PORT}`:'')
-    config.withCredentials = true;
+    config.withCredentials = setting.HTTP.WITH_CREDENTIALS;
     config.timeout = setting.HTTP.TIMEOUT
     config.headers['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
     config.data = Qs.stringify(config.data);
@@ -26,6 +26,7 @@ axios.interceptors.response.use(function (response) {
         if(code === 888){
             
         }
+        console.log('error',data.message)
         // 对响应错误做点什么
         return Promise.reject(new Error(data.message));
     }
@@ -36,23 +37,11 @@ axios.interceptors.response.use(function (response) {
 
 export default {
     post(url,params = {}){
-        return  new Promise((resolve,reject)=>{
-            axios.post(url, params).then((data)=>{
-                resolve(data)
-            }).catch((error)=>{
-                reject(error);
-            })
-        })
+        return axios.post(url, params);
     },
     get(url,params = {}){
-        return new Promise((resolve,reject)=>{
-            let paramsStr = Qs.stringify(params)
-            url = `${url}${paramsStr? '?' + paramsStr : ''}`
-            axios.get(url).then((data)=>{
-                resolve(data)
-            })
-        }).catch(function (error) {
-            reject(error);
-        })
+        let paramsStr = Qs.stringify(params)
+        url = `${url}${paramsStr? '?' + paramsStr : ''}`
+        return axios.get(url);
     }
 }
